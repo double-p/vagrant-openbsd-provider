26b0d0e111ec142d3e595c6b2a8dce30f4fdc451
Philipp Buehler <pb@rootnexus.net>
2017-12-02 21:58:03 +0100

    add execute() from v-bhyve to short/simplify Subprocess

---
+++ lib/vagrant-openbsd-provider/executor.rb
@@ -0,0 +1,38 @@
+require "vagrant/util/busy"
+require "vagrant/util/subprocess"
+
+module VagrantPlugins
+  module ProviderOpenBSD
+    module Executor
+      # This class is used to execute commands as subprocess.
+      class Exec
+        # When we need the command's exit code we should set parameter
+        # exit_code to true, otherwise this method will return executed
+        # command's stdout
+        def execute(exit_code, *cmd, **opts, &block)
+          # Append in the options for subprocess
+          cmd << { notify: [:stdout, :stderr] }
+          cmd.unshift('sh', '-c')
+
+          interrupted = false
+          # Lambda to change interrupted to true
+          int_callback = ->{ interrupted = true }
+          result = ::Vagrant::Util::Busy.busy(int_callback) do
+            ::Vagrant::Util::Subprocess.execute(*cmd, &block)
+          end
+
+          return result.exit_code if exit_code
+
+          result.stderr.gsub!("\r\n", "\n")
+          result.stdout.gsub!("\r\n", "\n")
+
+          if result.exit_code != 0 || interrupted
+            raise Errors::ExecuteError
+          end
+
+          result.stdout[0..-2]
+        end
+      end
+    end
+  end
+end
